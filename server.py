b749157bd54aa7f9aa0693bfccc151e91b11fb1e
diegojavier132 <115697956+diegojavier132@users.noreply.github.com>
2022-12-11 22:17:52 +0800

    Update server.py

--- server.py
+++ server.py
@@ -29,32 +29,44 @@ while True:
     message = json.loads(string_data)
 
     # Check the type of the message
-    if message["type"] == "join":
+    if message["command"] == "join":
         # A client is joining the chatroom
         # Store the client's handle, IP address, and port number
         clients[message["handle"]] = (addr[0], addr[1])
         print("Joined")
-    elif message["type"] == "leave":
+        
+    elif message["command"] == "leave":
         # A client is leaving the chatroom
         # Remove the client from the dictionary
         del clients[message["handle"]]
-    elif message["type"] == "register":
+    elif message["command"] == "register":
         # A client is registering a handle
         # Store the client's handle, IP address, and port number
         clients[message["handle"]] = (addr[0], addr[1])
+
+        message_to_send = {
+            "command": "register",
+            "handle": message["handle"],
+            "message": f'Welcome {message["handle"]}!',
+        }
+
+        message_to_send_str = json.dumps(message_to_send)
+        for client in clients.values():
+            sock.sendto(message_to_send_str.encode(), client)
         print(f'{message["handle"]} has registered')
-    elif message["type"] == "all":
+    elif message["command"] == "all":
         # A client is sending a message to all clients
         # Increment the message ID
         next_message_id += 1
 
         # Create the message to be sent to all clients
         message_to_send = {
-            "type": "message",
+            "command": "all",
             "id": next_message_id,
             "handle": message["handle"],
             "message": message["message"],
         }
+        
 
         # Encode the message as a JSON formatted string
         message_to_send_str = json.dumps(message_to_send)
@@ -62,16 +74,24 @@ while True:
         # Send the message to all registered clients
         for client in clients.values():
             sock.sendto(message_to_send_str.encode(), client)
-    elif message["type"] == "msg":
+    elif message["command"] == "msg":
         # A client is sending a message to a specific handle
         # Check if the handle exists in the dictionary
-        if message["to"] in clients:
+        if message["handle"] in clients:
             # Increment the message ID
             next_message_id += 1
 
             # Create the message to be sent to the specific handle
             message_to_send = {
-                "type": "message",
+                "command": "msg",
+                "id": next_message_id,
+                "handle": message["from"],
+                "message": message["message"],
+                
+            }
+
+            message_to_client = {
+                "command": "msgr",
                 "id": next_message_id,
                 "handle": message["handle"],
                 "message": message["message"],
@@ -80,7 +100,9 @@ while True:
 
             # Encode the message as a JSON formatted string
             message_to_send_str = json.dumps(message_to_send)
+            message_to_client_str = json.dumps(message_to_client)
 
             # Send the message to the specific handle
-            sock.sendto(message_to_send_str.encode(), clients[message["to"]])
+            sock.sendto(message_to_send_str.encode(), clients[message["handle"]])
+            sock.sendto(message_to_client_str.encode(), clients[message["from"]])
 
