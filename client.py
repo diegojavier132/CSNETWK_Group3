11914c86da591e0aa92a528bd6452bbad4900c61
diegojavier132 <115697956+diegojavier132@users.noreply.github.com>
2022-12-11 13:48:33 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,75 +1,122 @@
+import json
 import socket
 import threading
-import random
-import re
-import json
 
-command = '{"command":"test", "handle":"why", "message":"*^8tjqdkb", "ip":"   ", "port":"  "}'
-command_json = json.loads(command)
+# The client handle
+handle = ""
 
+# The server IP address and port number
+SERVER_IP = "localhost"
+SERVER_PORT = 4353
 
+# The maximum size of a message, in bytes
+MAX_MESSAGE_SIZE = 1024
 
-def receive():
-    global command_json
+# Create the UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# A function to receive messages in a separate thread
+def receive_messages():
     while True:
         try:
-            message, addr = client.recvfrom(1024)
-            json_string = message.decode()
-            command_json = json.loads(json_string)
-            if command_json["command"] == "join":
-                print("Sucessful Connection")
+            # Receive a message from the server
+            data, addr = sock.recvfrom(1024)
+            string = data.decode()
+            # Decode the JSON formatted string
+            message = json.loads(string)
 
+            # Check the type of the message
+            if message["type"] == "message":
+                # Print the message to the screen
+                print(f"{message['handle']}: {message['message']}")
         except:
             pass
 
-message = input("")
-
-if message.startswith("/join"): 
-    res = message.split()
-    command_json["command"] = "join"
-    command_json["ip"] = res[1]
-    command_json["port"] = res[2]
-    server_ip = command_json["ip"]
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client.bind((server_ip, int(command_json["port"])))
-    t = threading.Thread(target=receive)
-    t.start()
-
-    result = json.dumps(command_json)
-    client.sendto(result.encode(), (server_ip, 9999))
+# Start the thread to receive messages
 
 
 while True:
-    message = input("")
+    # Read the user's input
+    user_input = input()
 
-    if message.startswith("/register"):
-        res = message.split()
-        command_json["command"] = "register"
-        command_json["handle"] = res[1]
-        message = json.dumps(command_json)
-        client.sendto(message.encode(), (server_ip, 9999))
+    # Split the input into words
+    words = user_input.split()
 
-    elif message.startswith("/all"):
-        res = message.split()
-        command_json["command"] = "all"
-        command_json["message"] = re.sub(r'.', '', message, count = 5)
-        command_json["message"] = f'{command_json["handle"]}: {command_json["message"]}'
-        result = json.dumps(command_json)
-        client.sendto(result.encode(), (server_ip, 9999))
+    # Check the first word of the input
+    if words[0] == "/join":
+        # The user is trying to join the chatroom
+        # Set the server IP address and port number
+        SERVER_IP = words[1]
+        SERVER_PORT = words[2]
+        sock.bind((SERVER_IP, int(SERVER_PORT)))
+        receive_thread = threading.Thread(target=receive_messages)
+        receive_thread.start()
+        
+    elif words[0] == "/leave":
+        # The user is trying to leave the chatroom
+        # Create the message to be sent to the server
+        message = {
+            "type": "leave",
+            "handle": handle,
+        }
 
-    elif message.startswith("/message"):
-        res = message.split()
-        command_json["command"] = "message"
-        command_json["handle"] = res[1]
-        command_json["message"] = res[2]
-        message = json.dumps(command_json)
-        client.sendto(message.encode(), (server_ip, 9999))
+        # Encode the message as a JSON formatted string
+        message_str = json.dumps(message)
 
+        # Send the message to the server
+        sock.sendto(message_str.encode(), (SERVER_IP, 9999))
 
-    else:
-        print("Invalid Command")
+        # Break out of the loop
+        break
+    elif words[0] == "/register":
+        # The user is trying to register a handle
+        # Set the handle
+        handle = words[1]
 
+        # Create the message to be sent to the server
+        message = {
+            "type": "register",
+            "handle": handle,
+        }
 
+        # Encode the message as a JSON formatted string
+        message_str = json.dumps(message)
 
+        # Send the message to the server
+        sock.sendto(message_str.encode(), (SERVER_IP, 9999))
+    elif words[0] == "/all":
+        # The user is trying to send a message to all clients
+        # Join the words of the input into a single string
+        message = " ".join(words[1:])
 
+        # Create the message to be sent to the server
+        message_to_send = {
+            "type": "all",
+            "handle": handle,
+            "message": message,
+        }
+
+        # Encode the message as a JSON formatted
+
+        message_to_send_str = json.dumps(message_to_send)
+
+        # Send the message to the server
+        sock.sendto(message_to_send_str.encode(), (SERVER_IP, 9999))
+
+    elif words[0] == "/msg":
+        message = " ".join(words[2:])
+
+            # Create the message to be sent to the server
+        message_to_send = {
+              "type": "msg",
+              "handle": handle,
+              "to": words[1],
+              "message": message,
+            
+        }
+
+        # Encode the message as a JSON formatted string
+        message_to_send_str = json.dumps(message_to_send)
+
+        # Send the message to the server
+        sock.sendto(message_to_send_str.encode(), (SERVER_IP, 9999))
